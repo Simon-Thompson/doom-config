6d1143bebd1ae16404fb1525f50f7dfeaae06f9f
Simon-Thompson <simonthompson@hotmail.ca>
2019-07-04 12:20:03 -0400

    First doom commit from magit

---
+++ config.el
@@ -0,0 +1,34 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+;; Set gs SPC to target all visible windows with avy
+(setq avy-all-windows t)
+
+;; Set todo keywords
+(add-hook! org-load
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "POTATO(n)" "WAIT(w@/!)" "|" "DONE(d)" "CANC(c)"))))
+
+;; Define twine-mode syntax highlighting
+(defvar twine-mode-hook nil)
+(add-to-list 'auto-mode-alist '("\\.tw\\'" . twine-mode))
+
+(defconst twine-font-lock-keywords
+  (list
+   '("^\\(::.+\\)" . font-lock-function-name-face) ; ::Passage
+   '("\\(\\[\\(.+\\)\\]\\)" . font-lock-keyword-face) ; [[foo]]
+   '("\\(<<.+>>\\)" . font-lock-builtin-face))) ; <<bar>>
+
+(defvar twine-dev-env "twee -t sugarcane")
+
+(defun twine-mode ()
+  (interactive)
+  (kill-all-local-variables)
+  (visual-line-mode)
+  (set (make-local-variable 'font-lock-defaults) '(twine-font-lock-keywords))
+  (setq major-mode 'twine-mode)
+  (setq mode-name "twine")
+  (run-hooks 'twine-mode-hook))
+
+(provide 'twine-mode)
